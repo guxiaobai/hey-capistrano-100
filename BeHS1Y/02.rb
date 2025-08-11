4735b82bb1075a6f98a38ee317c9c81da28fde3e
guxiaobai <sikuan.gu@gmail.com>
2025-08-11 12:25:21 +0800

    rbenv

---
+++ BeHS1Y/02.rb
@@ -0,0 +1,17 @@
+
+gem_group :development do
+   gem 'capistrano-rbenv', '~> 2.2'
+end
+
+# Capistrano::Rbenv
+uncomment_lines 'Capfile', /require.*rbenv/
+
+append_to_file 'config/deploy.rb' do
+  <<-EOS.strip_heredoc
+
+  # rbenv
+  set :rbenv_type, :user
+  set :rbenv_ruby, '3.3.8'
+  EOS
+end
+
